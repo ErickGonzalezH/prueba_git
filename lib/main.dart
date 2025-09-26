508b26e963a7fb2785a6ffed42c7139a3bc1a181
ErickGonzalezH <egonzalezh31@gmail.com>
2025-09-26 15:30:40 -0600

    commit prueba rama erick

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Prueba Github',
+      title: 'Prueba Github (Rama de erick)',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Prueba Github Home Page'),
+      home: const MyHomePage(title: 'Prueba Github (Rama de erick) Home Page'),
     );
   }
 }
